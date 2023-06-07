a416bdaa2c92fad7976d08091b87fc0923e8de7e
DrSqua <dr.squa@gmail.com>
2023-06-07 15:14:14 +0200

    Switched event class to interface

--- src/app/shared/models/items/events.ts
+++ src/app/shared/models/items/events.ts
@@ -1,15 +1,15 @@
 import {Item} from "./item";
 
-export class EventPreviewModel {
-  item!: Item;
-  start_date!: string;
-  end_date!: string;
-  location!: string;  // TODO Location Object, package maybe?
-  image_landscape!: string;
-  image_square!: string;
-  button_text!: string;
-  main_color!: "000000000";
-  text_color!: "#000000"
+export interface EventPreviewModel {
+  item: Item;
+  start_date: string;
+  end_date: string;
+  location: string;  // TODO Location Object, package maybe?
+  image_landscape: string;
+  image_square: string;
+  button_text: string;
+  main_color: "000000000";
+  text_color: "#000000"
 }
 
 export interface EventModelInterface {
