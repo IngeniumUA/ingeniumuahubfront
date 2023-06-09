27293315166eb867e798e86f629ee15bd023c9da
DrSqua <dr.squa@gmail.com>
2023-06-09 23:29:40 +0200

    Added account navbar

--- src/app/shared/components/account/account-navbar/account-navbar.component.ts
+++ src/app/shared/components/account/account-navbar/account-navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
     selector: 'app-account-navbar',
     templateUrl: './account-navbar.component.html',
     styleUrls: ['./account-navbar.component.css'],
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    NgIf,
+    NgClass
+  ],
     standalone: true
 })
 export class AccountNavbarComponent {
-
+  @Input() alignedBottom: boolean = true;
 }
