7618a624cbccb30a7a8916a09d89dbd352c2d09a
DrSqua <dr.squa@gmail.com>
2023-06-14 15:12:42 +0200

    Auto unsubscribe on valuechange in form and added @Output via eventemitter

--- src/app/shared/components/items/products/product/product.component.ts
+++ src/app/shared/components/items/products/product/product.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {ProductDataI} from "../../../../models/items/products";
 import {NgIf, NgStyle} from "@angular/common";
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-product',
@@ -14,10 +15,10 @@ import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
     ReactiveFormsModule,
   ]
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
   @Input() product!: ProductDataI;
   @Input() primaryColorFull: string = "#0a1f44";
-  @Output() selected: number = 4;
+  @Output() countEventEmitter: EventEmitter<number> = new EventEmitter<number>();
 
   productForm = this.formBuilder.group({
     count: [0, Validators.min(0)]
@@ -29,25 +30,26 @@ export class ProductComponent implements OnInit {
     this.productForm.get('count')?.addValidators(Validators.max(this.product.max_count));
 
     // Form input correction
-    this.InputCorrection()
+    this.ValueChangePipeline();
   }
 
-  InputCorrection() {
+  ValueChangePipeline() {
     // Get count field and detect valuechange, then subscribe to every change
-    this.productForm.get('count')?.valueChanges.subscribe((value: number | null) => {
+    this.productForm.get('count')?.valueChanges.pipe(
+      takeUntil(this.ngUnsubscribe)) // Unsubscribe behaviour
+      .subscribe((value: number | null) => {
         value = value ?? 0; // If null, set to 0
+
+        this.countEventEmitter.emit(value); // Emit Value
 
         if (value < 0) { // Set to 0 if value is negative
           this.productForm.patchValue({
             count: 0
-          })
-        }
-
+          })}
         if (this.product.max_count < value) { // Set to max_count if value is higher
           this.productForm.patchValue({
             count: this.product.max_count
-          })
-        }
+          })}
       }
     )
   }
@@ -62,5 +64,11 @@ export class ProductComponent implements OnInit {
     this.productForm.patchValue({
       count: (this.productForm.get("count")!.value ?? 0) - 1
     })
+  }
+
+  private ngUnsubscribe = new Subject<void>();
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
