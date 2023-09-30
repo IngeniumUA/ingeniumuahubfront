55189d57b1ed24bf831cad79cf609d7301d635f9
Sigfried Seldeslachts <me@sigfriedseldeslachts.be>
2023-09-30 19:06:56 +0200

    Updated pop up z page

--- src/app/features/public/popupz/popupz.component.ts
+++ src/app/features/public/popupz/popupz.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { CartService } from 'src/app/core/services/shop/cart/cart.service';
+import { EventItemDetailI } from 'src/app/shared/models/items/events';
+import { IItem } from 'src/app/shared/models/items/IItem';
+import { IProductItem } from 'src/app/shared/models/items/products/products';
+import { apiEnviroment } from 'src/enviroments';
 
 @Component({
   selector: 'app-page',
@@ -6,5 +13,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./popupz.component.scss']
 })
 export class PopupzComponent {
+  category: 'food' | 'drinks' | 'snacks' = 'food';
+  products$: Observable<IProductItem[]> = of([]);
+  event: EventItemDetailI = {} as EventItemDetailI;
 
+  constructor(private httpClient: HttpClient, private cartService: CartService) {}
+
+  ngOnInit() {
+    this.getProducts();
+    this.httpClient.get<EventItemDetailI>(apiEnviroment.apiUrl + "popup/event").subscribe((item) => {
+      this.event = item;
+    });
+  }
+
+  setCategory(category: 'food' | 'drinks' | 'snacks'): void {
+    this.category = category;
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.products$ = this.httpClient.get<IProductItem[]>(apiEnviroment.apiUrl + "popup/" + this.category);
+  }
+
+  addProduct(product: IProductItem): void {
+    this.cartService.setProductCount(this.event.item, product, 1);
+  }
 }
