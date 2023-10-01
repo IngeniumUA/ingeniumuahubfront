910fb7deac7b23a9017795796c2945a29dd177e1
DrSqua <dr.squa@gmail.com>
2023-10-01 15:14:44 +0200

    Added popupz order to account transactions

--- src/app/features/public/account/account-transactions/account-transactions.component.ts
+++ src/app/features/public/account/account-transactions/account-transactions.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import {AccountService, TransactionI} from "../../../../core/services/user/account/account.service";
 import {Observable, of} from "rxjs";
 import {CheckoutI} from "../../../../shared/components/items/interactions/checkout";
+import {HttpClient} from "@angular/common/http";
+import {apiEnviroment} from "../../../../../enviroments";
+
+interface Order {
+  order_no: number
+  status: boolean
+}
 
 @Component({
   selector: 'app-account-transactions',
@@ -9,8 +16,11 @@ import {CheckoutI} from "../../../../shared/components/items/interactions/checko
   styleUrls: ['./account-transactions.component.scss']
 })
 export class AccountTransactionsComponent {
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService,
+              private httpClient: HttpClient) {
   }
 
   checkouts$: Observable<CheckoutI[]> = this.accountService.getTransactions()
+
+  popUpZOrders$: Observable<Order[]> = this.httpClient.get<Order[]>(apiEnviroment.apiUrl + "popup/cache/user")
 }
