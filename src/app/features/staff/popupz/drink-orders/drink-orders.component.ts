f2e04fef57b94a278f0efad0b867838a65f01941
Sigfried Seldeslachts <me@sigfriedseldeslachts.be>
2023-10-01 12:40:32 +0200

    Created drinks page, updated template of public orders page

--- src/app/features/staff/popupz/drink-orders/drink-orders.component.ts
+++ src/app/features/staff/popupz/drink-orders/drink-orders.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/core/services/user/account/account.service';
+import { CheckoutI } from 'src/app/shared/components/items/interactions/checkout';
 
 @Component({
-  selector: 'app-drink-orders',
+  selector: 'app-page',
   templateUrl: './drink-orders.component.html',
   styleUrls: ['./drink-orders.component.css']
 })
 export class DrinkOrdersComponent {
+  constructor(private accountService: AccountService) {}
+  orders$: Observable<CheckoutI[]> = this.accountService.getTransactions();
 
+  public removeOrder(product: any): void {
+    // TODO: remove order
+  }
 }
