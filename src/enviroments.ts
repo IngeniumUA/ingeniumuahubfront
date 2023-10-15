19e2b18d0c6f69e5e05eb18fbc5cea0b1027ae4c
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-10-15 23:49:14 +0200

    Switched to using enviroment.json because of weird bugs in dev-build

--- src/enviroments.ts
+++ src/enviroments.ts
@@ -1,6 +1,7 @@
 // import * as apiEnv from "src/enviroment.prod.json";
+import * as apiEnv from "src/enviroment.json"
 
 export const apiEnviroment = {
-  apiEnv: {"apiUrl": "https://hub.ingeniumua.be/api/v1/"}
+  apiEnv: apiEnv
   //apiEnv: {"apiUrl": "http://127.0.0.1:8000/api/v1/"}
 };
