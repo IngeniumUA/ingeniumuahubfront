b75f9cade51c51344aada26150de267bc94a5363
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-10-15 23:34:24 +0200

    Failed to get enviroment variables working. Using old system for now

--- src/enviroments.ts
+++ src/enviroments.ts
@@ -1,9 +1,6 @@
-import * as apiEnv from "src/enviroment.prod.json"
-
-export interface ApiEnvI {
-  apiUrl: string
-}
+import * as apiEnv from "src/enviroment.prod.json";
 
 export const apiEnviroment = {
-  apiEnv: apiEnv
-}
+  apiEnv: {"apiUrl": "https://hub.ingeniumua.be/api/v1/"}
+  //apiEnv: {"apiUrl": "http://127.0.0.1:8000/api/v1/"}
+};
