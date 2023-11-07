fef7802ca7e73c5f1b834d6c568754f07468e9ae
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-11-07 16:26:08 +0100

    Cleaned up and added local ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,3 @@
 export const apiEnviroment = {
-  // apiUrl: "https://hub.dev.ingeniumua.be/api/v1/"
   apiUrl: "https://hub.dev.ingeniumua.be/api/v1/"
-  // apiUrl: "http://127.0.0.1:8000/api/v1/"
-  //apiEnv: {"apiUrl": "http://127.0.0.1:8000/api/v1/"}
 };
