c712e0a006f4d1d0434b8d1b99a5b25d09e462de
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-11-10 17:26:48 +0100

    Partners in json file

--- src/app/shared/components/partners/partner-grid/partner-grid.component.ts
+++ src/app/shared/components/partners/partner-grid/partner-grid.component.ts
@@ -3,24 +3,12 @@ import {RecSysItemPreviewComponent} from "../../items/recsys/rec-sys-item-previe
 import {AsyncPipe, NgForOf} from "@angular/common";
 import {RecSysPreviewI} from "../../../models/items/recsys_interfaces";
 import {HttpClient} from "@angular/common/http";
-import {Observable, of} from "rxjs";
-
-const PARTNERS: RecSysPreviewI[] = [
-  {
-    follow_through_link: '/partner/sparklink',
-    name: 'Sparklink',
-    date: undefined,
-    color: '255255255',
-    text_color: '00000000',
-    image_square: "https://storage.googleapis.com/ingeniumuahubbucket/hub/partner_logo/23_24/sparklink.webp",
-    image_landscape: 'null',
-  }
-]
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-partner-grid',
   templateUrl: './partner-grid.component.html',
-  styleUrls: ['./partner-grid.component.css'],
+  styleUrls: ['./partner-grid.component.scss'],
   imports: [
     RecSysItemPreviewComponent,
     NgForOf,
@@ -33,6 +21,6 @@ export class PartnerGridComponent {
   constructor(private httpClient: HttpClient) {
   }
 
-  partners: Observable<RecSysPreviewI[]> = of(PARTNERS);
+  partners: Observable<RecSysPreviewI[]> = this.httpClient.get<RecSysPreviewI[]>("/assets/temp_partner_config/partners.json");
 
 }
