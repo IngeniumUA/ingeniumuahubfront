fc5a332aab8f4d28214091c4d47be6a0181c0778
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-11-18 16:09:52 +0100

    Access policy showing content

--- src/app/shared/components/staff_webmaster_manager/details/access-policy-detail/access-policy-detail.component.ts
+++ src/app/shared/components/staff_webmaster_manager/details/access-policy-detail/access-policy-detail.component.ts
@@ -1,10 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {StaffAccessPolicyI} from "../../../../models/staff/staff_access_policy";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-access-policy-detail',
   templateUrl: './access-policy-detail.component.html',
   styleUrls: ['./access-policy-detail.component.css'],
+  imports: [
+    JsonPipe
+  ],
   standalone: true
 })
 export class AccessPolicyDetailComponent {
