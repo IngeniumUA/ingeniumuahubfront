ce909170995631e85d49d84607cbd960df996af3
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-11-18 20:59:13 +0100

    Better style for hubproduct

--- src/app/shared/components/staff_webmaster_manager/details/product-blueprint-detail/product-blueprint-detail.component.ts
+++ src/app/shared/components/staff_webmaster_manager/details/product-blueprint-detail/product-blueprint-detail.component.ts
@@ -1,13 +1,18 @@
 import {Component, Input} from '@angular/core';
 import {StaffProductBlueprintI} from "../../../../models/staff/staff_productblueprint";
+import {DatePipe} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-product-blueprint-detail',
   templateUrl: './product-blueprint-detail.component.html',
   styleUrls: ['./product-blueprint-detail.component.css'],
+  imports: [
+    DatePipe,
+    RouterLink
+  ],
   standalone: true
 })
 export class ProductBlueprintDetailComponent {
   @Input() productBlueprint!: StaffProductBlueprintI
-
 }
