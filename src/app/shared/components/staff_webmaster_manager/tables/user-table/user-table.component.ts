13908fde1051aff14cd09c1aa5edc8d97d173715
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-12-18 19:34:49 +0100

    Big improvement to user table

--- src/app/shared/components/staff_webmaster_manager/tables/user-table/user-table.component.ts
+++ src/app/shared/components/staff_webmaster_manager/tables/user-table/user-table.component.ts
@@ -1,33 +1,44 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableModule} from "@angular/material/table";
-import {Observable, of} from "rxjs";
+import {debounceTime, delay, Observable, of} from "rxjs";
 import {AsyncPipe, DatePipe, NgIf} from "@angular/common";
 import {StaffUserService} from "../../../../../core/services/staff/staff-user-service";
 import {StaffUserDetailI} from "../../../../models/staff/staff_user_detail";
 import {RouterLink} from "@angular/router";
 import {MatPaginator, MatPaginatorModule, PageEvent} from "@angular/material/paginator";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {distinctUntilChanged} from "rxjs/operators";
 
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.scss'],
-  imports: [
-    MatTableModule,
-    AsyncPipe,
-    NgIf,
-    RouterLink,
-    DatePipe,
-    MatPaginatorModule,
-    MatProgressSpinnerModule
-  ],
+    imports: [
+        MatTableModule,
+        AsyncPipe,
+        NgIf,
+        RouterLink,
+        DatePipe,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
   standalone: true
 })
 export class UserTableComponent implements OnInit, AfterViewInit {
-
-  displayedColumns = ["uuid", "prefered_mail", "password_set", "lid", "is_staff", "is_manager", "modified_at"]
-
   userData$: Observable<StaffUserDetailI[]> = of([])
+  userStats$: Observable<number> = of(0)
+
+  searchForm = new FormGroup({
+      uuidControl: new FormControl(''),
+      emailControl: new FormControl(''),
+  })
+
+  GetDisplayedColumns(): string[] {
+      return ["uuid", "email", "password_set", "lid", "is_staff", "is_manager", "last_login", "modified_at"]
+  }
 
   constructor(private staffUserService: StaffUserService) {
   }
@@ -35,7 +46,16 @@ export class UserTableComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit() {
-    this.LoadData()
+    this.LoadData();
+    this.searchForm.valueChanges.pipe(
+        delay(500),
+        distinctUntilChanged((prev, next) => prev.emailControl === next.emailControl),
+        debounceTime(500)
+        //combineLatest
+    ).subscribe((value) => {
+            this.LoadData()
+        }
+    )
   }
 
   ngAfterViewInit() {
@@ -48,12 +68,22 @@ export class UserTableComponent implements OnInit, AfterViewInit {
     paginatorIntl.lastPageLabel = '';
   }
 
-  LoadData(event: PageEvent | null = null) {
-    if (event === null) {
-      this.userData$ = this.staffUserService.getUsers()
-      return
-    }
-    this.paginator!.pageIndex = event.pageIndex
-    this.userData$ = this.staffUserService.getUsers(event.pageIndex * event.pageSize, event.pageSize)
+  LoadData(pageEvent: PageEvent | null = null) {
+    // Form parsing
+    const emailControlValue = this.searchForm.get('emailControl')!.value;
+    const uuidControlValue = this.searchForm.get('uuidControl')!.value;
+
+    const emailQuery = emailControlValue === '' ? null: emailControlValue;
+    const uuidQuery = uuidControlValue === '' ? null: uuidControlValue;
+
+    // Page behaviour
+    const pageIndex = pageEvent === null ? 0: pageEvent.pageIndex;
+    const pageSize = pageEvent === null ? 100: pageEvent.pageSize;
+
+    // Data
+    this.userData$ = this.staffUserService.getUsers(pageIndex * pageSize, pageSize, uuidQuery, emailQuery)
+
+    // Stats
+    this.userStats$ = this.staffUserService.getUserStats(pageIndex * pageSize, pageSize, uuidQuery, emailQuery)
   }
 }
