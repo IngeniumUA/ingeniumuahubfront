f0cd7773a1bc83a8c26c41b28ffe074b5a7ccb9d
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-12-19 19:18:14 +0100

    Added currencypipe

--- src/app/shared/components/staff_webmaster_manager/tables/checkout-table/checkout-table.component.ts
+++ src/app/shared/components/staff_webmaster_manager/tables/checkout-table/checkout-table.component.ts
@@ -10,23 +10,25 @@ import {AsyncPipe, DatePipe, NgClass, NgForOf, NgIf} from "@angular/common";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatTableModule} from "@angular/material/table";
 import {RouterLink} from "@angular/router";
+import {CurrencyPipe} from "../../../../pipes/currency.pipe";
 
 @Component({
   selector: 'app-checkout-table',
   templateUrl: './checkout-table.component.html',
   styleUrls: ['./checkout-table.component.scss'],
-  imports: [
-    AsyncPipe,
-    DatePipe,
-    MatPaginatorModule,
-    MatProgressSpinnerModule,
-    MatTableModule,
-    NgForOf,
-    NgIf,
-    ReactiveFormsModule,
-    NgClass,
-    RouterLink
-  ],
+    imports: [
+        AsyncPipe,
+        DatePipe,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatTableModule,
+        NgForOf,
+        NgIf,
+        ReactiveFormsModule,
+        NgClass,
+        RouterLink,
+        CurrencyPipe
+    ],
   standalone: true
 })
 export class CheckoutTableComponent {
--- src/app/shared/components/staff_webmaster_manager/tables/transaction-table/transaction-table.component.ts
+++ src/app/shared/components/staff_webmaster_manager/tables/transaction-table/transaction-table.component.ts
@@ -10,6 +10,7 @@ import {RouterLink} from "@angular/router";
 import {StatusStatsI} from "../../../../models/stats/transactionStats";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {distinctUntilChanged} from "rxjs/operators";
+import {CurrencyPipe} from "../../../../pipes/currency.pipe";
 
 @Component({
   selector: 'app-transaction-table',
@@ -26,7 +27,8 @@ import {distinctUntilChanged} from "rxjs/operators";
     NgForOf,
     NgClass,
     NgStyle,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CurrencyPipe
   ],
   standalone: true
 })
