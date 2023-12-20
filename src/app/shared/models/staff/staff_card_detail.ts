5b86a72cb3a32c69e0b2eadf2c42ec5baca24556
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-12-20 22:59:34 +0100

    Card table improved

--- src/app/shared/models/staff/staff_card_detail.ts
+++ src/app/shared/models/staff/staff_card_detail.ts
@@ -2,6 +2,7 @@ export interface StaffCardDetailI {
   id: string
   academic_year: string
   user_id: string | null
+  user_email: string | null
   card_type: string
   card_nr: number
   linked_date: string | null
