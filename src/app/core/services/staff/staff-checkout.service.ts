a9e992e14874b147e7095d045d3c7ee6127e89bf
WebmasterIngeniumUA <111612009+WebmasterIngeniumUA@users.noreply.github.com>
2023-12-23 21:26:46 +0100

    Fixed staff_service not assembling query string correctly in some cases. IE "?&item="

--- src/app/core/services/staff/staff-checkout.service.ts
+++ src/app/core/services/staff/staff-checkout.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {apiEnviroment} from "../../../../environments/environment";
-import {StaffUserDetailI} from "../../../shared/models/staff/staff_user_detail";
-import {StaffTransactionI} from "../../../shared/models/staff/staff_transaction";
 import {StatusStatsI} from "../../../shared/models/stats/transactionStats";
 import {StaffCheckoutI, StaffCheckoutPatchI} from "../../../shared/models/staff/staff_checkout";
 
@@ -48,10 +46,13 @@ export class StaffCheckoutService {
     user_id: string | null = null): Observable<StatusStatsI> {
     let query_str = "?"
     if (item !== null) {
-      query_str += "&item="+item;
+      query_str += "item="+item;
     }
     if (user_id !== null) {
-      query_str += "&user_id="+user_id;
+      if (item !== null) {
+        query_str += "&";  // Temp solution pending a better queryforming function
+      }
+      query_str += "user_id="+user_id;
     }
 
     return this.httpClient.get<StatusStatsI>(
