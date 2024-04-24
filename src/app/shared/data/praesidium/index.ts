888635222ca186ba41c5e83fc45b3ba397cea775
Sigfried <me@sigfriedseldeslachts.be>
2024-04-24 09:55:47 +0200

    Fixed incorrect file import in Praesidium page, lazy loading praesidium images.

--- src/app/shared/data/praesidium/index.ts
+++ src/app/shared/data/praesidium/index.ts
@@ -5,7 +5,7 @@ import _year2223 from "./praesidium_22-23.json";
 import _year2122 from "./praesidium_21-22.json";
 import _year2021 from "./praesidium_20-21.json";
 import _year1920 from "./praesidium_19-20.json";
-import _year1819 from "./praesidium_19-20.json";
+import _year1819 from "./praesidium_18-19.json";
 
 export default {
   '23-24': _year2324 as unknown as PraesidiumGroupI[],
