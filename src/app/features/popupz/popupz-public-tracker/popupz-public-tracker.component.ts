2544ab5cfeb2cc8780204b79da0139b0274303ea
Sigfried Seldeslachts <me@sigfriedseldeslachts.be>
2024-09-30 11:36:10 +0200

    Updated public tracker endpoint

--- src/app/features/popupz/popupz-public-tracker/popupz-public-tracker.component.ts
+++ src/app/features/popupz/popupz-public-tracker/popupz-public-tracker.component.ts
@@ -1,5 +1,15 @@
-import {AfterViewInit, Component, ElementRef, Inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, OnDestroy, ViewChild} from '@angular/core';
 import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {catchError, interval, Subscription, take} from "rxjs";
+import {HubCheckoutTrackerStatusEnum} from "@ingenium/app/shared/models/tracker";
+import {apiEnviroment} from "@ingenium/environments/environment";
+import {map} from "rxjs/operators";
+import {HttpClient} from "@angular/common/http";
+
+interface PublicOrderTrackerI {
+  id: number;
+  checkout_tracker_status: HubCheckoutTrackerStatusEnum,
+}
 
 @Component({
   selector: 'app-popupz-public-tracker',
@@ -13,23 +23,50 @@ import {NgForOf, NgOptimizedImage} from "@angular/common";
   ],
   templateUrl: './popupz-public-tracker.component.html',
 })
-export class PopupzPublicTrackerComponent implements AfterViewInit {
+export class PopupzPublicTrackerComponent implements AfterViewInit, OnDestroy {
   @ViewChild('inprogress') inProgress: ElementRef = null!;
   @ViewChild('completed') completed: ElementRef = null!;
 
   previousTimestamp: DOMHighResTimeStamp = 0;
   movingDirection: number[] = [0, 0]; // -1 = up, 0 = available, 1 = down, 2 = WAIT
-  inProgressOrder: number[] = [];
+  interval!: Subscription;
+  inProgressOrders: PublicOrderTrackerI[] = [];
+  doneOrders: PublicOrderTrackerI[] = [];
 
-  constructor(@Inject(Window) private readonly window: Window) {
-    // Create 1000 random integers from 1 to 1000
-    for (let i = 0; i < 100; i++) {
-      this.inProgressOrder.push(Math.floor(Math.random() * 1000) + 1);
-    }
-  }
+  constructor(@Inject(Window) private readonly window: Window, private httpClient: HttpClient) {}
 
   ngAfterViewInit() {
-    this.scrollLists(0)
+    this.getOrders();
+    this.interval = interval(5000)
+      .pipe()
+      .subscribe(() => {
+        this.getOrders();
+      });
+
+    this.scrollLists(0);
+  }
+
+  ngOnDestroy() {
+    this.interval.unsubscribe();
+  }
+
+  getOrders() {
+    this.httpClient.get<PublicOrderTrackerI[]>(`${apiEnviroment.apiUrl}checkout/tracker`)
+      .pipe(
+        take(1),
+        map((data: PublicOrderTrackerI[]) => {
+          this.inProgressOrders = data.filter((order) => {
+            return order.checkout_tracker_status === HubCheckoutTrackerStatusEnum.Pending;
+          });
+          this.doneOrders = data.filter((order) => {
+            return order.checkout_tracker_status === HubCheckoutTrackerStatusEnum.Ready;
+          });
+        }),
+        catchError((error) => {
+          return error;
+        })
+      )
+      .subscribe()
   }
 
   scrollLists(timestamp: DOMHighResTimeStamp) {
