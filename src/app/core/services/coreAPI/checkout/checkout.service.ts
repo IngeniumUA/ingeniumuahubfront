d4722e08f09370a9ff47142c6ff8475505c69c46
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-10-12 13:59:18 +0200

    Reworked checkout table for new api

---
+++ src/app/core/services/coreAPI/checkout/checkout.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {apiEnviroment} from '@ingenium/environments/environment';
+import {CheckoutI, CheckoutPatchI} from "@ingenium/app/shared/models/checkout/checkoutModels";
+import {StatusStatsI} from "@ingenium/app/shared/models/stats/transactionStats";
+import {StaffCheckoutI} from "@ingenium/app/shared/models/staff/staff_checkout";
+import {PaymentStatusEnum} from "@ingenium/app/shared/models/payment/statusEnum";
+
+export function removeNull<T>(obj: T | any) {
+  Object.keys(obj).forEach((key) => {
+    if (obj[key] && typeof obj[key] === 'object') removeNull(obj[key])
+    else if (obj[key] == null) delete obj[key]
+  })
+  return obj
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+  constructor(private httpClient: HttpClient) { }
+
+  apiUrl = apiEnviroment.apiUrl + 'checkout';
+
+  public queryCheckouts(offset: number = 0, count: number = 50,
+                        item: number | null = null,
+                        user: string | null = null,
+                        status: PaymentStatusEnum | null = null,
+                        user_email: string | null = null,
+                        checkout_id: string | null = null): Observable<CheckoutI[]> {
+    const param = {
+      offset: offset,
+      "limit": count,
+      "item": item,
+      "user": user,
+      "checkout_status": status,
+      "user_email": user_email,
+      "checkout_uuid": checkout_id
+    }
+    const params = new URLSearchParams(removeNull(param));
+    return this.httpClient.get<CheckoutI[]>(`${this.apiUrl}?${params.toString()}`);
+  }
+
+
+
+  public checkoutCountGroupByStatus(item: number | null = null,
+                                    user: string | null = null,
+                                    status: string | null = null,
+                                    user_email: string | null = null,
+                                    checkout_id: string | null = null): Observable<StatusStatsI> {
+    const param = {
+      "item": item,
+      "user": user,
+      "checkout_status": status,
+      "user_email": user_email,
+      "checkout_uuid": checkout_id
+    }
+    const params = new URLSearchParams(removeNull(param));
+    return this.httpClient.get<StatusStatsI>(`${this.apiUrl}/group_by?${params.toString()}`);
+  }
+
+  public patchCheckout(checkout_id: string, patchObj: CheckoutPatchI) {
+    return this.httpClient.patch<CheckoutI>(this.apiUrl + '/' + checkout_id, patchObj);
+  }
+}
--- src/app/shared/models/checkout/checkoutModels.ts
+++ src/app/shared/models/checkout/checkoutModels.ts
@@ -1,9 +1,29 @@
 import {TransactionInI} from "@ingenium/app/shared/models/transaction/transaction_models";
 
+export interface CheckoutI {
+  checkout_uuid: string
+
+  user_uuid: string
+  user_email: string
+
+  payment_provider: number
+
+  amount: number
+  note: string | null
+
+  completed_timestamp: string | null
+  last_updated_timestamp: string
+  created_timestamp: string
+}
+
 export interface CheckoutInI {
   user_email: string;
   payment_provider: number
 
   transactions: TransactionInI[]
   note: number | null
+}
+
+export interface CheckoutPatchI {
+  checkout_status: number
 }
