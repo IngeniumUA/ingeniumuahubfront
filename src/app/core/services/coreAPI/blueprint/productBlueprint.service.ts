1a67372ce9868b711fcf87c0bf0678c45b9a918e
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-10-12 19:48:29 +0200

    Fixed blueprint dashboard, started converting project from old staffblueprint service to new blueprint service

---
+++ src/app/core/services/coreAPI/blueprint/productBlueprint.service.ts
@@ -0,0 +1,59 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {apiEnviroment} from '@ingenium/environments/environment';
+import {CheckoutI, CheckoutPatchI} from "@ingenium/app/shared/models/checkout/checkoutModels";
+import {StatusStatsI} from "@ingenium/app/shared/models/stats/transactionStats";
+import {PaymentStatusEnum} from "@ingenium/app/shared/models/payment/statusEnum";
+import {removeNull} from "@ingenium/app/core/services/serviceUtils";
+import {
+  ProductBlueprintI,
+  ProductBlueprintInI
+} from "@ingenium/app/shared/models/product_blueprint/productBlueprintModels";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductBlueprintService {
+  constructor(private httpClient: HttpClient) { }
+
+  apiUrl = apiEnviroment.apiUrl + 'blueprint';
+
+  public queryProductTable(source_item_id: number | null = null,
+                           origin_item_id: number | null = null,
+                           transaction_status: PaymentStatusEnum | null = null): Observable<[]> {
+    const param = {
+      source_item_id: source_item_id,
+      origin_item_id: origin_item_id,
+      transaction_status: transaction_status
+    }
+    const params = new URLSearchParams(removeNull(param));
+    return this.httpClient.get<[]>(`${this.apiUrl}/table?${params.toString()}`);
+  }
+
+  public queryProductBlueprints(offset: number = 0, count: number = 50,
+                                source_item_id: number | null = null,
+                                origin_item_id: number | null = null): Observable<ProductBlueprintI[]> {
+    const param = {
+      offset: offset,
+      limit: count,
+      source_item_id: source_item_id,
+      origin_item_id: origin_item_id,
+    }
+    const params = new URLSearchParams(removeNull(param));
+    return this.httpClient.get<ProductBlueprintI[]>(`${this.apiUrl}?${params.toString()}`);
+  }
+
+  public getProductBlueprint(product_blueprint_id: number): Observable<ProductBlueprintI> {
+    return this.httpClient.get<ProductBlueprintI>(`${this.apiUrl}/${String(product_blueprint_id)}`);
+  }
+
+  public postProductBlueprint(postObj: ProductBlueprintInI) {
+    return this.httpClient.post<ProductBlueprintI>(`${this.apiUrl}`, postObj);
+  }
+
+  public putProductBlueprint(product_blueprint_id: number, putObj: ProductBlueprintI) {
+    return this.httpClient.put<ProductBlueprintI>(`${this.apiUrl}/${String(product_blueprint_id)}`, putObj);
+  }
+}
