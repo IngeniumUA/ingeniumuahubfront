e1cdde0422261a29e2e15ef26d9030640822f5f9
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-10-18 10:52:12 +0200

    improved removeNull to also remove empty lists

--- src/app/core/services/serviceUtils.ts
+++ src/app/core/services/serviceUtils.ts
@@ -1,7 +1,21 @@
-export function removeNull<T>(obj: T | any) {
+export function removeNull<T>(obj: T | any): T | any {
   Object.keys(obj).forEach((key) => {
-    if (obj[key] && typeof obj[key] === 'object') removeNull(obj[key])
-    else if (obj[key] == null) delete obj[key]
+    if (obj[key] && typeof obj[key] === 'object') {
+      // Recursively call removeNull for nested objects
+      removeNull(obj[key]);
+    }
+    // If the object is an empty array, delete the key
+    else if (Array.isArray(obj[key]) && obj[key].length === 0) {
+      delete obj[key];
+    }
+    // If the object is an empty object, delete the key
+    else if (Object.keys(obj[key]).length === 0) {
+      delete obj[key];
+    }
+    // Delete if the value is null or undefined
+    else if (obj[key] == null) {
+      delete obj[key];
+    }
   })
-  return obj
+  return obj;
 }
