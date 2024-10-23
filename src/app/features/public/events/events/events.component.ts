860972aca47193e6797f59d140f2f1b59f0c211a
Sigfried Seldeslachts <me@sigfriedseldeslachts.be>
2024-10-23 21:07:21 +0200

    Remove unused imports

--- src/app/features/public/events/events/events.component.ts
+++ src/app/features/public/events/events/events.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import {catchError, Observable} from 'rxjs';
-import {LayoutService} from '../../../../core/services/layout/layout.service';
-import {RecSysPreviewI} from '../../../../shared/models/items/recsys_interfaces';
+import {Observable} from 'rxjs';
+import {RecSysPreviewI} from '@ingenium/app/shared/models/items/recsys_interfaces';
 import {EventService} from "@ingenium/app/core/services/coreAPI/item/derived_services/event.service";
 import {HttpState} from "@ingenium/app/shared/models/httpState";
 
@@ -13,7 +12,5 @@ import {HttpState} from "@ingenium/app/shared/models/httpState";
 export class EventsComponent {
   events$: Observable<HttpState<RecSysPreviewI[]>> = this.eventService.getEventsList();
 
-  constructor(private eventService: EventService) {
-    //this.events$ = this.eventService.getEventsList()
-  }
+  constructor(private eventService: EventService) { }
 }
--- src/app/features/public/shop/shop/shop-home.component.ts
+++ src/app/features/public/shop/shop/shop-home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs';
 import {RecSysPreviewI} from '../../../../shared/models/items/recsys_interfaces';
-import {LayoutService} from '../../../../core/services/layout/layout.service';
 import {ShopService} from "@ingenium/app/core/services/coreAPI/item/derived_services/shop.service";
 import {Store} from "@ngxs/store";
 import {CartState} from "@ingenium/app/core/store";
