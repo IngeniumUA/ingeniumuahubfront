08f42507261b92f039234f4e03dd20898903a225
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-11-15 21:31:28 +0100

    Abstracted StatusToStats method from transaction and checkout components. Added recheck validity button to transaction table.

--- src/app/shared/models/stats/transactionStats.ts
+++ src/app/shared/models/stats/transactionStats.ts
@@ -1,3 +1,5 @@
+import {PaymentStatusEnum} from "@ingenium/app/shared/models/payment/statusEnum";
+
 export interface StatusStatsI {
   0: number  // All
   1: number  // Successful
@@ -7,4 +9,26 @@ export interface StatusStatsI {
   5: number  // Refund Pending
   6: number  // Refunded
   7: number  // Partially refunded
+}
+
+export function StatusToStats(status: number, statsObject: StatusStatsI): number {
+  if (status === 0) {  // '0' is all
+    return statsObject[PaymentStatusEnum.all];
+  }
+  else if (status === PaymentStatusEnum.successful) {
+    return statsObject[PaymentStatusEnum.successful];
+  }
+  else if (status === PaymentStatusEnum.failed) {
+    return statsObject[PaymentStatusEnum.failed];
+  }
+  else if (status === PaymentStatusEnum.cancelled) {
+    return statsObject[PaymentStatusEnum.cancelled]
+  }
+  else if (status == PaymentStatusEnum.refunded || status === PaymentStatusEnum.partially_refunded) {
+    return statsObject[PaymentStatusEnum.refunded] + statsObject[PaymentStatusEnum.partially_refunded];
+  }
+  else if (status === PaymentStatusEnum.pending  || status === PaymentStatusEnum.refund_pending) {
+    return statsObject[PaymentStatusEnum.pending] + statsObject[PaymentStatusEnum.refund_pending];
+  }
+  return 0;
 }
