a2d83cc3435e5480fa22028ac9362beb8e8a8b76
kippie <wout.de.smit@hotmail.be>
2024-11-22 13:42:14 +0100

    started implementation for a redirect for wallet

--- src/app/core/services/coreAPI/file.service.ts
+++ src/app/core/services/coreAPI/file.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {apiEnviroment} from '@ingenium/environments/environment';
+import {removeNull} from "@ingenium/app/core/services/serviceUtils";
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,30 @@ export class MediabucketFileService {
   constructor(private httpClient: HttpClient) {
   }
 
-  apiUrl = apiEnviroment.apiUrl + 'file/media';
+  apiUrl = apiEnviroment.apiUrl + 'file';
 
   public listFileNames(): Observable<string[]> {
-    return this.httpClient.get<string[]>(`${this.apiUrl}/list`);
+    return this.httpClient.get<string[]>(`${this.apiUrl}/media/list`);
   }
+
+  public getWalletLinks(transaction_uuid: string,
+                        banner_link: string,
+                        event_name: string,
+                        end_date: string,
+                        start_date: string,
+                        nummer: number,
+                        locatie_naam: string): Observable<{google_wallet_link: string, apple_wallet_link: string}>{
+    const param = {
+      transaction_uuid: transaction_uuid,
+      banner_link: banner_link,
+      event_name: event_name,
+      end_date: end_date,
+      start_date: start_date,
+      nummer: nummer,
+      locatie_naam: locatie_naam
+    }
+    const params = new URLSearchParams(removeNull(param));
+    return this.httpClient.get<{google_wallet_link: string, apple_wallet_link: string}>(`${this.apiUrl}/wallet?${params.toString()}`);
+  }
+
 }
