13497ec43aeae64245e084bc2f575b7ec32519de
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-11-25 10:21:17 +0100

    Added failedproduct interface to be used

--- src/app/shared/models/cart/cartI.ts
+++ src/app/shared/models/cart/cartI.ts
@@ -1,10 +1,23 @@
 import {CheckoutSmollI} from "@ingenium/app/shared/models/payment/checkout/hubCheckoutI";
+import {ValidityEnum} from "@ingenium/app/shared/models/payment/transaction/validityEnum";
 
 export interface CartSuccessI {
   checkout: CheckoutSmollI
   tracker_id: number | null
 }
 
+export interface FailedProductI {
+  index: number
+
+  available: boolean
+  validity: ValidityEnum
+
+  auth_error: object
+  non_auth_error: object
+
+  product: any
+}
+
 export interface CartFailedI {
-  failed_products: any[]
+  failed_products: FailedProductI[]
 }
