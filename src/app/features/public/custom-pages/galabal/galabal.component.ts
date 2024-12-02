5e2a2264f53bd2bfa4bba00bfcd3ac2c3c99b3e8
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-12-02 22:38:43 +0100

    Improved query for products in products.service.ts, started custom page for gala

--- src/app/features/public/custom-pages/galabal/galabal.component.ts
+++ src/app/features/public/custom-pages/galabal/galabal.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from "@ingenium/app/core/services/coreAPI/item/derived_services/event.service";
 import {EventItemWideI} from "@ingenium/app/shared/models/item/eventI";
+import {Observable} from "rxjs";
+import {HttpState} from "@ingenium/app/shared/models/httpState";
+import {ItemWideLimitedI} from "@ingenium/app/shared/models/item/itemwideI";
+import {ProductOutI} from "@ingenium/app/shared/models/product/products";
+import {ProductsService} from "@ingenium/app/core/services/coreAPI/products.service";
 
 @Component({
   selector: 'app-galabal',
@@ -9,28 +14,32 @@ import {EventItemWideI} from "@ingenium/app/shared/models/item/eventI";
 })
 export class GalabalComponent implements OnInit {
 
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService,
+              private productService: ProductsService) {
   }
 
+  products$: Observable<ProductOutI[]> = this.productService.getProducts("Ingenium Galabal");
+  // $galabalItem: Observable<HttpState<ItemWideLimitedI>> = this.eventService.getEvent("Ingenium%20Galabal");
+
   ngOnInit() {
-    this.eventService.getEvent("Ingenium%20Galabal").subscribe({
-      next: item => {
-        if (item.data === undefined || item.data === null) {
-          window.location.href = 'https://www.facebook.com/events/1660039801532627?acontext=%7B%22event_action_history%22%3A[]%7D';
-          return
-        }
-        const eventItem = item.data as EventItemWideI;
-        const url = eventItem.derived_type.display.follow_through_link
-        if (url.startsWith("http")) {
-          window.location.href = url;
-        } else {
-          window.location.href = `https://ingeniumua.be/${url}`;
-        }
-      },
-      error: (_: Error) => {
-        window.location.href = 'https://www.facebook.com/events/1660039801532627?acontext=%7B%22event_action_history%22%3A[]%7D';
-      }
-    })
+    // this.eventService.getEvent("Ingenium%20Galabal").subscribe({
+    //   next: item => {
+    //     if (item.data === undefined || item.data === null) {
+    //       window.location.href = 'https://www.facebook.com/events/1660039801532627?acontext=%7B%22event_action_history%22%3A[]%7D';
+    //       return
+    //     }
+    //     const eventItem = item.data as EventItemWideI;
+    //     const url = eventItem.derived_type.display.follow_through_link
+    //     if (url.startsWith("http")) {
+    //       window.location.href = url;
+    //     } else {
+    //       window.location.href = `https://ingeniumua.be/${url}`;
+    //     }
+    //   },
+    //   error: (_: Error) => {
+    //     window.location.href = 'https://www.facebook.com/events/1660039801532627?acontext=%7B%22event_action_history%22%3A[]%7D';
+    //   }
+    // })
   }
 
 }
