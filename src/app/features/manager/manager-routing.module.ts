ced4b874d72e82b727196ec3c38b7798ecc455a9
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2025-01-18 21:45:12 +0100

    Added interaction page to manager routing

--- src/app/features/manager/manager-routing.module.ts
+++ src/app/features/manager/manager-routing.module.ts
@@ -3,6 +3,9 @@ import {RouterModule, Routes} from '@angular/router';
 import {ManagerHomeComponent} from './managerhome/manager-home/manager-home.component';
 import {ManagerComponent} from './manager.component';
 import {FlagTableComponent} from './flag-table/flag-table.component';
+import {
+  InteractionsPageComponent
+} from "@ingenium/app/features/webmaster/interactions-page/interactions-page.component";
 
 
 const routes: Routes = [
@@ -12,6 +15,7 @@ const routes: Routes = [
       {path: '', component: ManagerHomeComponent },
       {path: 'flag', component: FlagTableComponent},
       {path: 'items', loadChildren: () => import('./items/manager-items.module').then(x => x.ManagerItemsModule)},
+      {path: 'interaction', component: InteractionsPageComponent},
     ]
   },
 ];
