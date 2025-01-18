d3ee766b6f10a0780bdecfcf907c48c4283144f9
kippie <wout.de.smit@hotmail.be>
2025-01-18 23:54:34 +0100

    implemented sentry properly

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as Sentry from "@sentry/angular";
+import * as Sentry from '@sentry/capacitor';
+import * as SentryAngular from '@sentry/angular';
 
 import { AppModule } from './app/app.module';
 import { apiEnviroment } from './environments/environment';
@@ -8,19 +9,26 @@ import { apiEnviroment } from './environments/environment';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 Sentry.init({
-  dsn: "https://50ffd26da050af7ec4e2122b7e5ab77a@o4507006131437568.ingest.us.sentry.io/4507006138777600",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/ingeniumua\.be\/api/, /^https:\/\/hub\.dev\.ingeniumua\.be\/api/],
-  // enabled: apiEnviroment.name !== 'development',
-  enabled: true, // disabled sentry in app cuz it no workey
-  environment: apiEnviroment.name,
-  release: apiEnviroment.versions.revision,
-});
+    dsn: "https://832eff534433afa7d7be27a930c422d4@o4507006131437568.ingest.us.sentry.io/4508667027783680",
+    integrations: [
+      SentryAngular.browserTracingIntegration(),
+    ],
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/ingeniumua\.be\/api/, /^https:\/\/hub\.dev\.ingeniumua\.be\/api/],
+    // enabled: apiEnviroment.name !== 'development',
+    enabled: true, // disabled sentry in app cuz it no workey
+    environment: apiEnviroment.name,
+    release: apiEnviroment.versions.revision,
+
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  },
+  // Forward the init method from @sentry/angular
+  SentryAngular.init
+);
 
 // Call the element loader before the bootstrapModule/bootstrapApplication call
 defineCustomElements(window).then();
