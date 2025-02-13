7ba5c378dde2a56f27e4a2cdbe81868a670bc3ad
Sigfried Seldeslachts <me@sigfriedseldeslachts.be>
2025-02-13 11:56:38 +0100

    Forgot one env variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,7 +15,7 @@ const OAuthConfig: AuthConfig = {
 export const apiEnviroment = {
   name: 'production',
   apiUrl: 'https://hub.ingeniumua.be/api/v1/',
-  appHost: 'ingeniumua.be',
+  appHost: 'admin.ingeniumua.be',
   oauthConfig: OAuthConfig,
   versions: versions,
   turnstileSiteKey: '0x4AAAAAAAX0Qzpta2sfvGK-',
