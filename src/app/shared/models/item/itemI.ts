128e9029a0a8528dbffaf18a6acc83ef9afcc122
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-02-24 22:20:01 +0100

    Improved item table. Added paginator. Added option to filter by item type

--- src/app/shared/models/item/itemI.ts
+++ src/app/shared/models/item/itemI.ts
@@ -3,6 +3,20 @@ import {
   AvailabilityCompositionInI
 } from "@ingenium/app/shared/models/item/availabilityCompositionI";
 
+export enum ItemTypeEnum {
+  none,
+  eventitem,
+  shopitem,
+  carditem,
+  promoitem,
+  linkitem,
+  notificationitem
+}
+
+export const ItemTypeList = [
+  ItemTypeEnum.none, ItemTypeEnum.eventitem, ItemTypeEnum.shopitem, ItemTypeEnum.carditem, ItemTypeEnum.linkitem, ItemTypeEnum.promoitem, ItemTypeEnum.notificationitem
+]
+
 export interface ItemLimitedInI {
   name: string
   description: string
