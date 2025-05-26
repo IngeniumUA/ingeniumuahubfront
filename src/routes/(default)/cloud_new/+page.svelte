1452c942bccf2fadbc0a24ce43983fea54a7c28f
kippie <wout.de.smit@hotmail.be>
2025-05-26 10:56:44 +0200

    added list

--- src/routes/(default)/cloud_new/+page.svelte
+++ src/routes/(default)/cloud_new/+page.svelte
@@ -391,7 +391,7 @@
         {:else if fileIsImg(openedFile.file)}
           <img src="{openedFile.url}" alt="Cloud">
         {:else if fileIsPdf(openedFile.file)}
-          <PdfRender pdfUrl={openedFile.blob}></PdfRender>
+          <PdfRender pdfBlob={openedFile.blob}></PdfRender>
         {:else}
           <embed title="cloud bestand" src={openedFile.url} width="100%" height="100%" style="min-height: 400px; border-radius: 5px">
         {/if}
@@ -429,7 +429,7 @@
               <button class="icon-text-{grid_mode ? 'grid' : 'list'}" onclick="{()=>{downloadAndOpenFile(file[1])}}">{file[0]}</button>
               {#if !grid_mode}
                 <div class="ml-auto pr-2">
-                  <button type="button" onclick="{()=>{downloadAndOpenFile(file[1])}}" class="button button-education button-icon button-icon-only cursor-pointer" aria-label="download">
+                  <button type="button" onclick="{()=>{downloadFile(file[1])}}" class="button button-education button-icon button-icon-only cursor-pointer" aria-label="download">
                     <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                       <path fill-rule="evenodd" d="M13 11.15V4a1 1 0 1 0-2 0v7.15L8.78 8.374a1 1 0 1 0-1.56 1.25l4 5a1 1 0 0 0 1.56 0l4-5a1 1 0 1 0-1.56-1.25L13 11.15Z" clip-rule="evenodd"/>
                       <path fill-rule="evenodd" d="M9.657 15.874 7.358 13H5a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.358l-2.3 2.874a3 3 0 0 1-4.685 0ZM17 16a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clip-rule="evenodd"/>
