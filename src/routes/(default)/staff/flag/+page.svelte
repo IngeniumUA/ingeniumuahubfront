4e23bd0b698c8da2411256985ee0406cfb825665
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-08-04 00:36:03 +0200

    Got form POST working. More prototyping done for the flag overview

--- src/routes/(default)/staff/flag/+page.svelte
+++ src/routes/(default)/staff/flag/+page.svelte
@@ -6,17 +6,96 @@
 		HubFlagTypeList,
 		HubFlagValueTypeEnum, HubFlagValueTypeList
 	} from '$lib/models/flag/HubFlagI';
+	import { PUBLIC_API_URL } from '$env/static/public';
+	import { getAuthorizationHeaders } from '$lib/auth/auth';
 
-	let addingNew = false;
-	let newFlagValueType = HubFlagValueTypeEnum.bool;
 	function toggleAddNew() {
 		addingNew = !addingNew;
 	}
+	let addingNew = false;
+	let newFlagError: string | null = null;
+	// Bindings
+	let newFlagName: string = '';
+	let newFlagType: HubFlagTypeEnum = HubFlagTypeEnum.configuration;
+	let newFlagValueType = HubFlagValueTypeEnum.bool;
+	// Might be terribly inefficient but hey, we're still learning
+	let booleanNewFlagValue: boolean = false;
+	let numberNewFlagValue: number = 0;
+	let stringNewFlagValue: boolean = false;
+
+	let loadingPost: boolean = false;
+
+	async function handleSubmit() {
+		// Resetting error and preventing double POST with flag
+		newFlagError = null;
+		loadingPost = true;
+
+		// Parsing flag
+		let parsedValue: { [key: string]: boolean | number | string };
+		switch (newFlagValueType) {
+			case HubFlagValueTypeEnum.bool: {
+				parsedValue = { value: booleanNewFlagValue };
+				break;
+			}
+			case HubFlagValueTypeEnum.int: {
+				parsedValue = { value: numberNewFlagValue };
+				break;
+			}
+			case HubFlagValueTypeEnum.string: {
+				parsedValue = { value: stringNewFlagValue };
+				break;
+			}
+			case HubFlagValueTypeEnum.dict: {
+				newFlagError = "Dict nog niet geimplementeerd";
+				loadingPost = false;
+				return;
+			}
+		}
+
+		const flagPost: Omit<HubFlag, 'id'> = {
+			name: newFlagName,
+			flag_type: newFlagType,
+			flag_value_type: newFlagValueType,
+			value: parsedValue
+		};
+
+		try {
+			const response = await fetch(`${PUBLIC_API_URL}/flag`, {
+				method: 'POST',
+				headers: getAuthorizationHeaders(null, { 'Content-Type': 'application/json' }),
+				body: JSON.stringify(flagPost)
+			});
+			if (response.ok) {
+				toggleAddNew();
+			} else {
+				newFlagError = `Failed to create flag: ${await response.text()}`;
+			}
+		} catch (error) {
+		newFlagError = error instanceof Error ? error.message : 'Error submitting form';
+		} finally {
+			loadingPost = false; // Reset loading state
+		}
+	}
+
 	export let data: {
 		configFlags: HubFlag[];
 		featureFlags: HubFlag[];
 	};
 </script>
+
+<style>
+	.ingenium-form {
+			fieldset {
+					@apply mb-2;
+					label {
+							@apply font-semibold text-blue-900;
+					}
+					p {
+							@apply text-sm text-gray-500;
+					}
+			}
+	}
+</style>
 
 <main class="ingenium-container relative" id="main-content">
 	<div class="flex justify-between items-center mb-6">
@@ -36,10 +115,22 @@
 		<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 auto-cols-fr">
 			{#each data.configFlags as flag (flag.id)}
 				<div class="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow">
-					<h3 class="text-lg font-semibold">
+					<div class="flex justify-between items-center">
+					<h3 class="font-semibold">
 						<span class="text-gray-600 font-semibold">{flag.id}: </span>
 						{flag.name}
 					</h3>
+					<svg fill="#1f2980" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+							 width="20px" height="20px" viewBox="0 0 528.899 528.899"
+							 xml:space="preserve">
+							<g>
+								<path d="M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981
+									c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611
+									C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069
+									L27.473,390.597L0.3,512.69z"/>
+							</g>
+							</svg>
+					</div>
 					<p class="text-sm text-gray-600">
 						{HubFlagValueTypeEnum[flag.flag_value_type]} {HubFlagTypeEnum[flag.flag_type]} flag
 					</p>
@@ -71,6 +162,15 @@
 					{:else}
 						Error
 					{/if}
+
+					<div class="flex justify-between items-center">
+						<button class="button button-danger w-24 button-inline">
+							<span class="text-white">Remove</span>
+						</button>
+						<button class="button button-primary w-24 button-inline">
+							<span class="text-white">Update</span>
+						</button>
+					</div>
 				</div>
 			{/each}
 		</div>
@@ -99,48 +199,50 @@
 		{#if addingNew}
 			<h2>Adding new</h2>
 
-			<form method="POST">
+			<form method="POST"
+						on:submit={handleSubmit}
+						class="ingenium-form">
 				<fieldset>
 					<div class="form-field">
-						<label for="newFlagName" class="font-semibold text-blue-900">Name</label>
-						<input id="newFlagName" type="text" required/>
-						<p class="text-sm text-gray-500">Unieke naam voor de flag. Let op, zelfs als je een flag uitschakelt kan je die naam niet meer gebruiken.</p>
+						<label for="newFlagName">Name</label>
+						<input id="newFlagName" type="text" required bind:value={ newFlagName }/>
+						<p>Unieke naam voor de flag. Let op, zelfs als je een flag uitschakelt kan je die naam niet meer gebruiken.</p>
 					</div>
 				</fieldset>
 
 				<fieldset>
 					<div class="form-field col-span-4">
-						<label for="newFlagType" class="font-semibold text-blue-900">Flag Type</label>
-						<select id="newFlagType" required>
+						<label for="newFlagType">Flag Type</label>
+						<select id="newFlagType" required bind:value={ newFlagType }>
 							{#each HubFlagTypeList as flagType}
 								<option value={ flagType }>{ HubFlagTypeEnum[flagType] }</option>
 							{/each}
 						</select>
-						<p class="text-sm text-gray-500">Welke soort flag het is. Zie hoger op de pagina voor uitleg.</p>
+						<p>Welke soort flag het is. Zie hoger op de pagina voor uitleg.</p>
 					</div>
 				</fieldset>
 
 				<fieldset>
 					<div class="form-field col-span-4">
-						<label for="newFlagValueType" class="font-semibold text-blue-900">Flag Value Type</label>
+						<label for="newFlagValueType">Flag Value Type</label>
 						<select id="newFlagValueType" required bind:value={ newFlagValueType }>
 							{#each HubFlagValueTypeList as flagValueType}
 								<option value={ flagValueType }>{ HubFlagValueTypeEnum[flagValueType] }</option>
 							{/each}
 						</select>
-						<p class="text-sm text-gray-500">De waarde van de flag kan verschillende vormen aannemen.</p>
+						<p>De waarde van de flag kan verschillende vormen aannemen.</p>
 					</div>
 				</fieldset>
 
 				<fieldset>
 					<div class="form-field col-span-4">
-						<label for="newFlagValue" class="font-semibold text-blue-900">Flag Value</label>
+						<label for="newFlagValue">Flag Value</label>
 						{#if newFlagValueType === HubFlagValueTypeEnum.bool}
-							Bool
+							<input id="newFlagValue" name="value" type="checkbox" bind:checked={booleanNewFlagValue}/>
 						{:else if newFlagValueType === HubFlagValueTypeEnum.int}
-							Int
+							<input id="newFlagValue" name="value" type="number" required bind:value={numberNewFlagValue}/>
 						{:else if newFlagValueType === HubFlagValueTypeEnum.string}
-							String
+							<input id="newFlagValue" name="value" type="text" required bind:value={stringNewFlagValue}/>
 						{:else if newFlagValueType === HubFlagValueTypeEnum.dict}
 							Object
 						{:else}
@@ -151,10 +253,11 @@
 
 				<div class="flex gap-4 items-center mb-6">
 					<button type="submit" class="button button-primary w-24 button-inline"
-									on:click={toggleAddNew}>
+									disabled={loadingPost}>
 						<span class="text-white">Submit</span>
 					</button>
 					<button class="button button-primary w-24 button-inline"
+									disabled={loadingPost}
 									on:click={toggleAddNew}>
 						<span class="text-white">Cancel</span>
 					</button>
@@ -167,4 +270,7 @@
 			</button>
 		{/if}
 	</div>
+	{#if newFlagError !== null}
+		<p class="error-message">{newFlagError}</p>
+	{/if}
 </main>
