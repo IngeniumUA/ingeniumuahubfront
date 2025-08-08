5eb917ef02ad0f80120c264a6b4d26116a9e78ed
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-08-09 00:05:54 +0200

    Prototyped an iframe embed

---
+++ src/lib/components/dashboard_iframe/dashboard_iframe.svelte
@@ -0,0 +1,30 @@
+﻿<script lang="ts">
+	export let token: string;
+	export let path: string = "";
+	let iframeEl: HTMLIFrameElement;
+
+	const baseUrl: string = "https://dashboard.ingeniumua.be";
+	$: iframeSrc = `${baseUrl}/${path}`;
+
+	function sendToken() {
+		if (iframeEl.contentWindow === null) {return}
+		iframeEl.contentWindow.postMessage(
+			{ type: "auth", token },
+			baseUrl
+		);
+	}
+
+	function onLoad() {
+		sendToken();
+	}
+</script>
+
+<iframe
+	bind:this={iframeEl}
+	src={iframeSrc}
+	title="Ingenium Dashboard"
+	width="100%"
+	height="800"
+	style="border:none;"
+	on:load={onLoad}
+></iframe>
