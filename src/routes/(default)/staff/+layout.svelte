6a383b218b30e08b1149063848dd8bc8c2b92bc8
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-08-24 22:50:00 +0200

    Folder changes

--- src/routes/(default)/staff/+layout.svelte
+++ src/routes/(default)/staff/+layout.svelte
@@ -25,8 +25,8 @@
 
 					<hr class="h-px bg-blue-900 border-0 dark:bg-blue-950">
 					<a href="/staff/item/event">Events</a>
-					<a href="/staff/vacatures">Vacatures</a>
-					<a href="/staff/lidkaarten">Lidkaarten</a>
+					<a href="/staff/item/vacatures">Vacatures</a>
+					<a href="/staff/item/lidkaarten">Lidkaarten</a>
 					<a href="/staff/item/notification">Email / App / SMS</a>
 
 				</div>
