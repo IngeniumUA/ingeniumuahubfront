84fb9f43c8579854ffe827ae3f0225c2248b7598
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-08-24 15:44:09 +0200

    Switched table display in event card to list of products and their success transactions. Also added recsys preview of event (might remove this later)

---
+++ src/lib/core_api/blueprint_api.ts
@@ -0,0 +1,17 @@
+﻿import { PUBLIC_API_URL } from '$env/static/public';
+import { getAuthorizationHeaders } from '$lib/auth/auth';
+
+export class CoreProductBlueprintAPI {
+	static async patchProductBlueprint(product_blueprint_identifier: string | number, patch_object: object): Promise<any> {
+		const res = await fetch(`${PUBLIC_API_URL}/blueprint/${product_blueprint_identifier}`, {
+			method: 'PATCH',
+			headers: getAuthorizationHeaders(null, { 'Content-Type': 'application/json' }),
+			body: JSON.stringify(patch_object)
+		});
+		if (res.ok) {
+			return await res.json();
+		} else {
+			throw `Failed to put blueprint: ${await res.text()}`;
+		}
+	}
+}
