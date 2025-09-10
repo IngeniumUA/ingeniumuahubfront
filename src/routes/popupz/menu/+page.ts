c3518e6afc9a18490039b34247341282dede6065
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-09-10 09:53:40 +0200

    Pop-up Z page now queries special pop-up z endpoint

--- src/routes/popupz/menu/+page.ts
+++ src/routes/popupz/menu/+page.ts
@@ -1,8 +1,12 @@
 ﻿import type { PageLoad } from './$types';
-import { CoreItemAPI } from '$lib/core_api/core_api';
+import { PUBLIC_API_URL } from '$env/static/public';
+import { getAuthorizationHeaders } from '$lib/auth/auth';
 
 export const load: PageLoad = async () => {
-	const products = await CoreItemAPI.queryProductsForItem("Pop-up Z 2025");
-
+	const res = await fetch(`${PUBLIC_API_URL}/popupz`, {
+		method: 'GET',
+		headers: getAuthorizationHeaders(null, { 'Content-Type': 'application/json' }),
+	});
+	const products = res.ok ? await res.json(): [];
 	return { products }
 }
