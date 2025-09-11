de5e6ed8e68ffdb376805ac250218a64bd006517
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-09-11 18:30:50 +0200

    Removed the SSR for staff pages

--- src/routes/(default)/staff/+layout.ts
+++ src/routes/(default)/staff/+layout.ts
@@ -1,5 +1,13 @@
-export const load = async ({ data }) => {
-	return {
-		...data,
+import { getLoginUrlWithRedirect, hasValidToken } from '$lib/auth/auth';
+import { hasRole } from '$lib/states/auth.svelte';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ params, url }) => {
+	if (!hasValidToken(params)) {
+		redirect(307, getLoginUrlWithRedirect(url.href));
 	}
+	if (!hasRole("staff")) {
+		redirect(308, "/")
+	}
+	return {}
 }
