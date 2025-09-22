ca7acb0445c7d3087cbc740a0e4cf269a0e4477d
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-09-22 10:28:21 +0200

    Open new tab added to cart click if webmaster

--- src/routes/popupz/menu/+page.svelte
+++ src/routes/popupz/menu/+page.svelte
@@ -6,6 +6,8 @@
 	import { slide } from 'svelte/transition';
 	import { makePretty } from '$lib/utilities/style-utilities';
 	import { addProductToCart, cartProducts, updateProductMetaForm } from '$lib/states/cart.svelte';
+	import { hasRole } from '$lib/states/auth.svelte';
+	import { goto } from '$app/navigation';
 
 	/**
 	 * Assigning data from load function in +page.svelte
@@ -75,6 +77,17 @@
 		successToast(`${productGrouped.product_blueprint_name} toegevoegd!`)
 	}
 
+	/**
+	 * Cart functions
+	 */
+	function handleCartClick() {
+		if (hasRole("webmaster")) {
+			// Open in a new tab
+			window.open('/shop/cart', '_blank');
+		} else {
+			goto('/shop/cart');
+		}
+	}
 </script>
 
 <style>
@@ -248,12 +261,12 @@
 	</div>
 	{/if}
 
-	<a href="/shop/cart" class="absolute bottom-4 right-4 text-white bg-blue-900 hover:bg-blue-950 focus:ring-4
+	<button onclick={handleCartClick} class="absolute bottom-4 right-4 text-white bg-blue-900 hover:bg-blue-950 focus:ring-4
 	focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-center
 	inline-flex items-center me-2 py-4 px-6">
 		<svg class="w-8 h-8 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
 			<path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
 		</svg>
-		<span class="text-lg">Naar Winkelkar</span>
-	</a>
+		<span class="text-lg">Naar Winkelkar ({cartProducts.length})</span>
+	</button>
 </main>
