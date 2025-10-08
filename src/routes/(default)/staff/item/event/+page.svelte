d36742d0a62aa09cfa3b56c4d730d8b061986cd5
Robbe De Helt <11708254+DrSqua@users.noreply.github.com>
2025-10-08 17:06:28 +0200

    Page refresh when you toggle onlyshowavailable

--- src/routes/(default)/staff/item/event/+page.svelte
+++ src/routes/(default)/staff/item/event/+page.svelte
@@ -20,6 +20,11 @@
 	 * Query parameter for showing available
 	 */
 	let onlyShowAvailable: boolean = $state(true);
+	$effect(() => {
+		if (onlyShowAvailable !== undefined) {
+			refresh();
+		}
+	});
 
 	/**
 	 * Refreshing all data on the page
