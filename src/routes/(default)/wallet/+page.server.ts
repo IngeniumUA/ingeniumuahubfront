f50af9e522608b5b22918688feafd92bb2a6f276
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2025-10-11 21:45:35 +0200

    Updated wallet redirects (apple gives a file instead of a url)

--- src/routes/(default)/wallet/+page.server.ts
+++ src/routes/(default)/wallet/+page.server.ts
@@ -8,28 +8,33 @@ export const load: PageServerLoad = async ({ params, url, fetch }) => {
   try {
     const platform = url.searchParams.get('platform');
     const uuid = url.searchParams.get('transaction_uuid');
-    const number = url.searchParams.get('nummer');
-    const location = url.searchParams.get('locatie_naam');
 
     // If any of these values are null, redirect to the homepage
-    if (!platform || !uuid || !number || !location) {
+    if (!platform || !uuid) {
       return redirect(307, '/');
     }
 
-    const redirectUrl: string = await fetch(
-      `${PUBLIC_API_URL}/account/wallet/${platform}?transaction_uuid=${uuid}&nummer=${number}&locatie_naam=${location}`,
-      {
-        method: 'GET',
-        headers: getAuthorizationHeaders(params),
-      }
-    ).then(handleRequest);
+		if (platform === "google") {
+			const redirectUrl: string = await fetch(`${PUBLIC_API_URL}/account/wallet/${platform}?transaction_uuid=${uuid}`,
+			{
+				method: "GET",
+				headers: getAuthorizationHeaders(params),
+			}).then(handleRequest);
 
-    return redirect(302, redirectUrl);
+			return redirect(302, redirectUrl);
+		}
+
+		// APPLE WALLET → pass data to +page.svelte for download
+		if (platform === "apple") {
+			return {
+				platform,
+				query: {transaction_uuid: uuid},
+			};
+		}
+
   } catch (e: any) {
-    if (e.status === 302) { // This is really vague that there are two ways to redirect.
-      return redirect(302, e.location || '/');
-    }
-
-    return redirect(307, '/');
-  }
+		if (e.status === 302) { // This is really vague that there are two ways to redirect.
+			return redirect(302, e.location || '/');
+			}
+	return redirect(307, '/'); }
 }
