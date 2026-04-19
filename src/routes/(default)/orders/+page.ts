4f0d69cc334149f8f0517516b06dc386fc41cc19
s0203301 <Robbe De Helt>
2026-04-19 20:01:16 +0200

    Public order router added

---
+++ src/routes/(default)/orders/+page.ts
@@ -0,0 +1,17 @@
+﻿import type { PageLoad } from './$types';
+import { PUBLIC_API_URL } from '$env/static/public';
+import { getAuthorizationHeaders } from '$lib/auth/auth';
+
+export const load: PageLoad = async ({ fetch, params }) => {
+	const ordersRes = await fetch(`${PUBLIC_API_URL}/order_tracking`, {
+		method: 'GET',
+		headers: getAuthorizationHeaders(params, { 'Content-Type': 'application/json' }),
+	});
+	const orders = ordersRes.ok ? await ordersRes.json(): [];
+	const itemRes = await fetch(`${PUBLIC_API_URL}/popupz`, {
+		method: 'GET',
+		headers: getAuthorizationHeaders(null, { 'Content-Type': 'application/json' }),
+	});
+	const item = itemRes.ok ? await itemRes.json(): [];
+	return { item, orders }
+}
